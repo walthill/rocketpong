2e1806042ad2b245e812e67e92f5d607e49f546c
Walter Hill <walterhilldev@gmail.com>
2021-06-18 22:20:10 -0500

    added logger and simplified it

---
+++ RocketEngine/src/core/RK_Log.cpp
@@ -0,0 +1,58 @@
+/********
+	=========================
+			 ROCKET ENGINE
+	=========================
+	File Created By: Walter Hill
+
+	This code is open source under the Apache 2.0 license.
+	(https://github.com/walthill/Rocket3D/blob/master/LICENSE)
+
+	=========================
+			 RK_Log.cpp
+	=========================
+
+********/
+
+
+#include "RK_Log.h"
+
+namespace RKTEngine
+{
+	std::shared_ptr<spdlog::logger> Log::msCoreLogger;
+	std::shared_ptr<spdlog::logger> Log::msClientLogger;
+
+	void Log::initialize()
+	{
+		std::string coutPattern = "%^[%T] %n >>> %v%$";
+		std::string foutPattern = "%^[%T] %n {%l} >>> %v%$";
+
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("RocketEngine.log", true));
+
+		logSinks[0]->set_pattern(coutPattern);
+		logSinks[1]->set_pattern(foutPattern);
+
+		msCoreLogger = std::make_shared<spdlog::logger>("ENGINE", begin(logSinks), end(logSinks));
+		spdlog::register_logger(msCoreLogger);
+		msCoreLogger->set_level(spdlog::level::trace);
+		msCoreLogger->flush_on(spdlog::level::trace);
+
+		msClientLogger = std::make_shared<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(msClientLogger);
+		msClientLogger->set_level(spdlog::level::trace);
+		msClientLogger->flush_on(spdlog::level::trace);
+	}
+
+	void Log::reportAllocs()
+	{
+		std::vector<std::string> reportList = RKTUtil::MemoryTracker::getInstance()->logAllocs();
+
+		RKT_CORE_INFO(reportList[0]);
+
+		for (auto it = reportList.begin() + 1; it != reportList.end(); ++it)
+		{
+			RKT_CORE_ERROR(it->c_str());
+		}
+	}
+}
---
+++ RocketEngine/src/core/RK_Log.h
@@ -0,0 +1,88 @@
+/********
+	=========================
+			 ROCKET ENGINE
+	=========================
+	File Created By: Walter Hill
+
+	Rocket3D is an open source 3D game engine written using C++ & OpenGL.
+
+	This class makes use of the spdlog logging library
+	(https://github.com/gabime/spdlog)
+
+	This code is open source under the Apache 2.0 license.
+	(https://github.com/walthill/Rocket3D/blob/master/LICENSE)
+
+	=========================
+			 RK_Log.h
+	=========================
+	This class is utlized as a static class. It incorporates spdlog
+	in order to output info and errors to the console and to log files.
+
+********/
+
+#ifndef RKT_LOG_H
+#define RKT_LOG_H
+
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
+#include <RKTUtils/MemoryTracker.h>
+
+namespace RKTEngine
+{
+	class Log
+	{
+		public:
+			/***
+				* Initializes loggers for console and file outputs
+			***/
+			static void initialize();
+
+			/***
+				* Report current memory allocations to console and file logs
+			***/
+			static void reportAllocs();
+
+			/***
+				* Cleanup console and file loggers
+			***
+			void cleanup();*/
+
+			/***
+				* Access console logger for engine core
+			***/
+			static std::shared_ptr<spdlog::logger>& getCoreLogger() { return msCoreLogger; };
+			/***
+				* Access console logger for game app
+			***/
+			static std::shared_ptr<spdlog::logger>& getClientLogger() { return msClientLogger; };
+
+		private:
+			static std::shared_ptr<spdlog::logger> msCoreLogger, msClientLogger;
+	};
+}
+
+//Init macro
+#define RKT_LOGGER_INIT()		::RKTEngine::Log::initialize();
+
+//Memory leak report macro
+#define RKT_MEMREPORT()			::RKTEngine::Log::reportAllocs();
+
+//Assertion wrapper
+#define RKT_ASSERT(...)			assert(__VA_ARGS__)
+
+// Core log macros
+#define RKT_CORE_TRACE(...)		::RKTEngine::Log::getCoreLogger()->trace(__VA_ARGS__)
+#define RKT_CORE_INFO(...)		::RKTEngine::Log::getCoreLogger()->info(__VA_ARGS__)
+#define RKT_CORE_WARN(...)		::RKTEngine::Log::getCoreLogger()->warn(__VA_ARGS__)
+#define RKT_CORE_ERROR(...)		::RKTEngine::Log::getCoreLogger()->error(__VA_ARGS__)
+#define RKT_CORE_CRITICAL(...)	::RKTEngine::Log::getCoreLogger()->critical(__VA_ARGS__)
+
+// Client log macros
+#define RKT_TRACE(...)			::RKTEngine::Log::getClientLogger()->trace(__VA_ARGS__)
+#define RKT_INFO(...)			::RKTEngine::Log::getClientLogger()->info(__VA_ARGS__)
+#define RKT_WARN(...)			::RKTEngine::Log::getClientLogger()->warn(__VA_ARGS__)
+#define RKT_ERROR(...)			::RKTEngine::Log::getClientLogger()->error(__VA_ARGS__)
+#define RKT_CRITICAL(...)		::RKTEngine::Log::getClientLogger()->critical(__VA_ARGS__)
+
+#endif // !RKT_Log
