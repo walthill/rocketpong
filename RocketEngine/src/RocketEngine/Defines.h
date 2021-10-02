95b646d016841546b9dd1a7c006b5c592e82d8b6
Walter Hill <walterhilldev@gmail.com>
2021-10-01 20:09:40 -0500

    adding profiling stubs and macro value

--- RocketEngine/src/RocketEngine/Defines.h
+++ RocketEngine/src/RocketEngine/Defines.h
@@ -4,6 +4,7 @@ namespace RKTEngine
 {
 	//Help from https://github.com/TheCherno/Hazel
 	//A game engine and tutorial series developed under the Apache-2.0 license
+	#define RKT_PROFILE 0
 	#define BITSHIFT(x) (1 << x)
 	#define RKT_BIND_MESSAGE_FN(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
 
--- RogueOne/src/main.cpp
+++ RogueOne/src/main.cpp
@@ -1,19 +1,24 @@
 #include <RocketEngine/core/Log.h>
+#include <RocketEngine/Defines.h>
+#include <RKTUtils/Profiling.h>
 #include "GameApp.h"
 
 int main()
 {
 	RKT_LOGGER_INIT();
-	RKT_CORE_INFO("Rocket Engine Core Logger initialized");
 
+	RKT_PROFILE_BEGIN_SESSION("Init", "profile_init.json");
 	GameApp::initInstance();
-
 	GameApp::getInstance()->initialize();
+	RKT_PROFILE_END_SESSION();
 
-	if (!GameApp::getInstance()->runGame())
-	{
-		GameApp::cleanInstance();
-	}
+	RKT_PROFILE_BEGIN_SESSION("Runtime", "profile_runtime.json");
+	GameApp::getInstance()->runGame();
+	RKT_PROFILE_END_SESSION();
+
+	RKT_PROFILE_BEGIN_SESSION("Cleanup", "profile_cleanup.json");
+	GameApp::cleanInstance();
+	RKT_PROFILE_END_SESSION();
 
 	RKT_MEMREPORT();
 
